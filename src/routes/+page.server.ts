5df3bc80f8a9623fab7e32fb78e00f420eca88e9
Rafal Pastuszak <rafal.pastuszak@gmail.com>
2023-04-16 13:23:09 +0100

    feat: render the news, add a cleaner layout, drop root

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,29 +1,69 @@
-import Parser from 'rss-parser';
+import { editorialTeam, type EditorialTeamMember } from '../data/editorial-team';
 
-type ArticleItem = {
-    title: string,
-    description: string,
-    pubDate: Date,
-    link: string,
+const  getDayOfYear = (date: Date) => {
+    const startOfYear = new Date(date.getFullYear(), 0, 0);
+    const diff = date.getTime() - startOfYear.getTime();
+    const oneDay = 1000 * 60 * 60 * 24;
+    const dayOfYear = Math.floor(diff / oneDay);
+  
+    return dayOfYear;
+  }
+
+const getBard = () => {
+	return editorialTeam[getDayOfYear(new Date()) % editorialTeam.length];
+};
+
+const makePrompt = (author: EditorialTeamMember, newsModern: any) => {
+	const prompt = `
+    You are ${author.name} – ${author.description}
+    Write a poem about the news events described below.
+    Make it not longer than 4 stanzas of 4 lines each.
+    The news is described using JSON format: {title, description, date}
+    Ensure that the style of your poem matches the style of the author you are impersonating.
+
+    === News events START ===
+    ${JSON.stringify(newsModern, null, 2)}
+    === News events END ===
+    `;
+
+    return prompt
+};
+
+import newsToday from '../data/today.json';
+
+const getNewsToday = () => {
+	return newsToday;
+};
+
+const getNewsMedieval = async (author: EditorialTeamMember, news: typeof newsToday['feedItems'] ) => {
+    const apiKey = process.env.OPENAI_API_KEY
+    const prompt = makePrompt(author, news)
+    const result = await fetch("https://api.openai.com/v1/chat/completions", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${apiKey}`,
+    },
+    body: JSON.stringify({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: prompt }],
+    }),
+  })
+    .then((response) => response.json())
+
+    return result.choices[0].message.content
 }
 
+const getSong = async () => {
+	const author = getBard();
+	const newsToday = getNewsToday();
+    const newsMedieval = await getNewsMedieval(author, newsToday.feedItems);
 
-async function fetchFeed(): Promise<ArticleItem[]> {
-  const parser = new Parser();
-  const feed = await parser.parseURL('https://www.npr.org/rss/rss.php?id=1001');
+	return { author, newsMedieval };
+};
 
-  return feed.items.map((item) => ({
-    title: item.title as string,
-    description: item.contentSnippet as string,
-    pubDate: new Date(item.pubDate as string),
-    link: item.link as string,
-  }));
-}
+export const load = async () => ({
+	song: await getSong()
+});
 
-
-
-export const load = async () => {
-    return {
-        feedItems: await fetchFeed()
-    }
-}
+export const prerender = true;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,147 @@
 <script lang="ts">
-    export let data = {feedItems: []}
+	export let data;
+
+	$: formattedNews = !data?.song?.newsMedieval
+		? []
+		: [...data.song.newsMedieval.split('\n\n')]
+				.slice(0, 5)
+				.map(
+					(stanza: string) => stanza.split('\n')
+						.map((line) => line.trim().split('')));
 </script>
-<h1>Greetings!</h1>
-<pre>
-    { JSON.stringify(data, null, 2) }
-</pre>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<article>
+	{#each formattedNews as paragraph, paragraphIndex}
+		<p class="stanza">
+			{#each paragraph as line, lineIndex}
+				{#each line as char, charIndex}
+					{#if charIndex === 0 && lineIndex === 0 && paragraphIndex === 0}
+						<span> {char} </span>
+					{:else}
+						<span
+							style="
+							display: inline-block;
+							transform: translateY({0.05 * Math.random() * 0.5 - 1}em);
+							color: hsl({Math.random() * 40 - 20}deg 60% 13.73%);
+							opacity: {0.9 - Math.random() * 0.2};
+						"
+						>
+							{char}
+						</span>
+					{/if}
+				{/each}
+				<br />
+			{/each}
+		</p>
+	{/each}
+	<p class="author">— {data.song.author.name}, {data.song.author.description}</p>
+</article>
+<a class="centaur" href="https://www.centaur-warns.com/"
+	><img src="/centaur.png" alt="a centaur" /></a
+>
+
+<div class="overlay" />
+
+<style>
+	@font-face {
+		font-family: 'font-default';
+		src: url('/fonts/default.woff2') format('woff2'), url('/fonts/default.woff') format('woff');
+		font-weight: normal;
+		font-style: normal;
+	}
+
+	@font-face {
+		font-family: 'font-drop-caps';
+		src: url('/fonts/drop-caps.woff2') format('woff2'), url('/fonts/drop-caps.woff') format('woff');
+		font-weight: normal;
+		font-style: normal;
+	}
+
+	:root {
+		--font-family: 'font-default', serif;
+		--font-family-drop-caps: 'font-drop-caps', serif;
+
+		--font-size-s: clamp(1rem, 0.3rem + 1vw, 2rem);
+		--font-size-m: clamp(1.25rem, 0.5rem + 2vw, 2rem);
+		--font-size-l: clamp(2.5rem, 2.2993rem + 0.8451vw, 3.25rem);
+	}
+
+	:global(*) {
+		box-sizing: border-box;
+	}
+
+	/* global style in svelte  */
+	:global(html, body) {
+		margin: 0;
+		padding: 0;
+	}
+
+	:global(body) {
+		font-size: var(--font-size-m);
+		/* background: url('/background.jpg') repeat center center fixed; */
+		background-color: #f9d395;
+		background-size: 60%;
+	}
+
+	.overlay {
+		position: fixed;
+		pointer-events: none;
+		content: '';
+
+		inset: 0;
+		z-index: 1;
+	}
+
+	article {
+		padding: 5rem;
+		line-height: 1;
+		position: relative;
+
+		color: #380e0e;
+		font-family: var(--font-family);
+
+		height: 100vh;
+		overflow: hidden;
+	}
+
+
+	article p:first-child {
+		margin-top: 0;
+	}
+
+	article p:first-child > span:first-child {
+		font-family: var(--font-family-drop-caps);
+		font-size: 2.85em;
+		scale: 1.15;
+		translate: 0.11em -.28em;
+		float: left;
+		line-height: 1;
+		color: #ac0303;
+		opacity: 0.8;
+		width: 1.05em;
+	}
+
+	.stanza {
+		white-space: break-spaces;
+		text-align: start;
+		font-size: calc(var(--font-size) * 1.4);
+	}
+
+	.author {
+		text-align: right;
+		font-size: var(--font-size-s);
+		max-width: 30rem;
+	}
+
+	.centaur {
+		position: absolute;
+		bottom: 2rem;
+		right: 2rem;
+		width: 10vw;
+	}
+
+	.centaur img {
+		width: 100%;
+		height: auto;
+	}
+</style>
